3c13d72cda8bba00af1ba4ec2636ba1fa3ff2dfc
pawel <pawel.maciejski@gmail.com>
2019-06-14 22:18:55 +0200

    [#1] add dump results to json

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import pprint
 
 from talpa.env import ensure_env
 from talpa.provider import AllegroProvider
-from talpa.utils import read_token
+from talpa.utils import read_token, dumps
 
 env = ensure_env()
 
@@ -19,3 +19,4 @@ payload = {
 ap = AllegroProvider(token=token_data['token'], base_url=BASE_URL)
 search_result = ap.search(payload)
 pprint.pprint(search_result)
+dumps('data/search_result.json', data=search_result)
--- talpa/utils.py
+++ talpa/utils.py
@@ -5,3 +5,8 @@ def read_token(file_name):
     with open(file_name, 'r') as fd:
         data = json.load(fd)
     return data
+
+
+def dumps(file_path, data):
+    with open(file_path, 'w') as fd:
+        json.dump(data, fd)
