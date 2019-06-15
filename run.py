563660fe69a07096eb9a90b88cc1458cd05cf449
pawel <pawel.maciejski@gmail.com>
2019-06-15 09:19:20 +0200

    [#1] move allegro queries to file and use schema

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+import json
 import pprint
 
 from talpa.env import ensure_env
 from talpa.provider import AllegroProvider
+from talpa.schema import SearchAllegroSchema
 from talpa.utils import read_token, dumps
 
 env = ensure_env()
@@ -10,13 +12,15 @@ with env.prefixed("TALPA_ALLEGRO_"):
     BASE_URL = env("BASE_URL")
 
 token_data = read_token('.tokens')
+search_schema = SearchAllegroSchema(strict=True, many=True)
 
-payload = {
-    'category.id': '93615',
-    'phrase': 'tenge',
-}
+queries = json.load(open('queries.json', 'r'))
+queries_loaded = search_schema.load(queries).data
+queries_parsed = search_schema.dump(queries_loaded).data
 
 ap = AllegroProvider(token=token_data['token'], base_url=BASE_URL)
-search_result = ap.search(payload)
-pprint.pprint(search_result)
-dumps('data/search_result.json', data=search_result, overwrite=False)
+
+for query in queries_parsed:
+    search_result = ap.search(query)
+    pprint.pprint(search_result)
+    dumps('data/search_result.json', data=search_result, overwrite=False)
