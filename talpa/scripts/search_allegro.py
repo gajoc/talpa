f5cf37261a12d668ab41c1feeaba3bf92f11a360
pawel <pawel.maciejski@gmail.com>
2019-06-15 18:38:20 +0200

    [#1] add closed auctions info in file name when dump allegro search result

--- talpa/scripts/search_allegro.py
+++ talpa/scripts/search_allegro.py
@@ -42,6 +42,9 @@ if __name__ == '__main__':
         result = ap.search(query)
         if 'error' in result:
             raise ValueError(f'got error when querying {query}, \nAPI response is\n{json.dumps(result, indent=4)}')
-        store_file_path = dumps(default_dump_file, data=result, overwrite=False)
+        store_file_path = dumps(default_dump_file,
+                                data=result,
+                                overwrite=False,
+                                extras='closed' if args.c else None)
         print(f'search result dumped in {store_file_path}')
     print(f'processed {len(queries_parsed)} queries.')
--- talpa/utils.py
+++ talpa/utils.py
@@ -9,12 +9,14 @@ def read_token(file_name):
     return data
 
 
-def dumps(file_path, data, overwrite):
+def dumps(file_path, data, overwrite, extras):
     final_path = str(file_path)
+
+    extras = '' if not extras else extras+'-'
 
     if not overwrite:
         root, ext = os.path.splitext(file_path)
-        final_path = ''.join([root, '-', str(uuid.uuid1()), ext])
+        final_path = ''.join([root, '-', extras, str(uuid.uuid1()), ext])
 
     with open(final_path, 'w') as fd:
         json.dump(data, fd)
