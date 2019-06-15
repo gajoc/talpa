e934f8fd54319ed37e7580d1594af47499f06416
pawel <pawel.maciejski@gmail.com>
2019-06-15 18:06:10 +0200

    [#1] add support for closed auctions

--- talpa/scripts/search_allegro.py
+++ talpa/scripts/search_allegro.py
@@ -14,6 +14,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument('queries_file', help='path to file with allegro queries in json format',
                 nargs='?',
                 default='../queries.json')
+ap.add_argument('-c', help='query for closed only auctions', action='store_true')
 args = ap.parse_args()
 
 
@@ -28,6 +29,12 @@ if __name__ == '__main__':
     queries = json.load(open(args.queries_file, 'r'))
     queries_loaded = search_schema.load(queries).data
     queries_parsed = search_schema.dump(queries_loaded).data
+
+    if args.c:
+        mode = 'CLOSED'
+        for q in queries_parsed:
+            q['searchMode'] = mode
+        print(f'searchMode is {mode}')
 
     ap = AllegroProvider(token=token, base_url=BASE_URL)
 
